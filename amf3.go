e24b22aedcec4f0028bdbd39bd091424431ced1c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-01-21 16:40:24 +0000

    feat: add AvmClass type

--- amf3.go
+++ amf3.go
@@ -3,3 +3,10 @@ package goAMF3
 type Reader interface {
 	Read(p []byte) (n int, err error)
 }
+
+type AvmClass struct {
+	name           string
+	dynamic        bool
+	externalizable bool
+	properties     []string
+}
